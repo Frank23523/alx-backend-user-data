f94d90722b9090c4b12dd2cfb8d0c964633965bb
Frank Anin <frankanin99@gmail.com>
2024-08-30 12:55:30 +0000

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,12 +69,13 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME', '')
 
     connection = mysql.connector.connect(
         user=username,
         password=password,
         host=host,
+        port=3306,
         database=db_name
     )
 
