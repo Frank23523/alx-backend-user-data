978db2a9ff1ec765964b40be9e3dd3fc9c73156d
Frank Anin <frankanin99@gmail.com>
2024-08-30 13:01:12 +0000

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,7 +69,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME', '')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+
+    if not db_name:
+        raise Exception("Database name not provided in environment variables.")
 
     connection = mysql.connector.connect(
         user=username,
