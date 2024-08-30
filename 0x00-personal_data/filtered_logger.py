2b430c13ff00f915dd111dba6af6132963ad9eaf
Frank Anin <frankanin99@gmail.com>
2024-08-30 10:42:39 +0000

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""filtered_logger.py"""
+
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+) -> str:
+    """Obfuscates specified fields in a log message"""
+    pattern = r'({})=[^{}]*'.format('|'.join(fields), separator)
+    return re.sub(pattern, r'\1=' + redaction, message)
