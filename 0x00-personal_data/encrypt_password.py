09d03f08861bc56abdb20f0f274a6eafd9a4dce2
Frank Anin <frankanin99@gmail.com>
2024-08-30 13:22:04 +0000

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,12 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password_bytes, salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate that the provided password matches the hashed password
+    """
+    password_bytes = password.encode('utf-8')
+
+    return bcrypt.checkpw(password_bytes, hashed_password)
