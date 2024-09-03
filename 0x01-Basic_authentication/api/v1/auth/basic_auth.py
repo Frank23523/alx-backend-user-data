e863e511e941a61861a7f4a1ae0cde589d2fe883
Frank Anin <frankanin99@gmail.com>
2024-09-03 23:21:02 +0000

    Basic - Allow password with ":"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -82,3 +82,25 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        if not auth_header:
+            return None
+
+        encoded = self.extract_base64_authorization_header(auth_header)
+        if not encoded:
+            return None
+
+        decoded = self.decode_base64_authorization_header(encoded)
+        if not decoded:
+            return None
+
+        email, password = self.extract_user_credentials(decoded)
+        if not email or not password:
+            return None
+
+        return self.user_object_from_credentials(email, password)
