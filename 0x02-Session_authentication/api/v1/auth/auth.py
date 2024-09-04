c445026ad4634b707b53d728ceb3984da9310cf3
Frank Anin <frankanin99@gmail.com>
2024-09-04 16:50:56 +0000

    copied from 0x01-Basic_Auth

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""auth.py"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """template for all authentication system to be implemented"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Returns True if the path is not in the list of strings excluded_paths
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        path = path.rstrip('/') + '/'
+
+        for excluded_path in excluded_paths:
+            excluded_path = excluded_path.rstrip('/') + '/'
+            if excluded_path.endswith('*/'):
+                if path.startswith(excluded_path[:-2]):
+                    return False
+            elif path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns the Authorization header from the request
+        """
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None - request will be the Flask request object
+        """
+        return None
